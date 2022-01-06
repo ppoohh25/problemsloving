e3558706660b5ac198dd4333a7c65a400a990b0c
ppoohh25 <poohrocker25@gmail.com>
2022-01-06 15:20:53 +0700

    .

--- Star Pattern/C++/Pattern01.cpp
+++ Star Pattern/C++/Pattern01.cpp
@@ -5,8 +5,20 @@
 ****
 *****
 */
-#include <stdio.h>
-int main(){
-printf("hello");
-return 0;
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1;i <= n ; i++){
+        for(int j = 1; j <= i; j++){
+            cout << '*';
+        };
+        cout << "\n";
+    };
+  
+    return 0;
 }
